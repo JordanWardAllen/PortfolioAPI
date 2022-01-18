b3dbfc885d11021be1d62cff26e0bbf38babb84a
Jordan Ward-Allen <jordan.mech@hotmail.com>
2022-01-18 17:54:31 +1000

    Added project files that were oddly deleted.

---
+++ src/test/java/com/portfolioAPI/portfolioapi/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioAPI.portfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
